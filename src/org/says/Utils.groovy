f90ebb550536d73d76d353507ada92fef667c0aa
Sergio Rodriguez <says.rodriguez@gmail.com>
2025-10-30 14:23:02 -0600

    test lolcommits

--- src/org/says/Utils.groovy
+++ src/org/says/Utils.groovy
@@ -1,10 +1,11 @@
 package org.says
 
+
 class Utils implements Serializable {
     staric String shortCommit(String hash) {
         
         if (hash == null) return ''
         return hash.take(7)
-        
+
     }
 }
