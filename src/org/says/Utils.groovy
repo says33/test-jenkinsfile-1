54ee5d2ce92acd105daedb44c426ca5b6c486e00
Sergio Rodriguez <says.rodriguez@gmail.com>
2025-10-30 13:00:29 -0600

    addin new utils class

---
+++ src/org/says/Utils.groovy
@@ -0,0 +1,10 @@
+package org.says
+
+class Utils implements Serializable {
+    staric String shortCommit(String hash) {
+        
+        if (hash == null) return ''
+        return hash.take(7)
+        
+    }
+}
